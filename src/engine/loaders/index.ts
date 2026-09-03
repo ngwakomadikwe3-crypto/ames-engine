25a18e2238d689c5d6bea43b50ccca58738570e1
ngwakomadikwe3-crypto <ngwakomadikwe3@gmail.com>
2026-09-03 20:18:49 +0200

    feat(engine): export jewelry asset loader

---
+++ src/engine/loaders/index.ts
@@ -0,0 +1,8 @@
+export { loadJewelryAsset } from './loadJewelryAsset'
+export { readJewelryScene } from './readJewelryScene'
+export type {
+  JewelryAssetReadResult,
+  JewelryAssetSummary,
+  JewelryMaterialInfo,
+  JewelryMeshInfo,
+} from './types'
